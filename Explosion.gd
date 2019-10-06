25679b2ce2465eecb28388df4c6151c403b9e5a1
Ciaran Woodward <ciaranwoodward@gmail.com>
2019-10-06 20:27:30 +0100

    Add explosion sound

--- Explosion.gd
+++ Explosion.gd
@@ -2,13 +2,16 @@ extends Node2D
 
 onready var p1 : Particles2D = get_node("Particles2D")
 onready var p2 : Particles2D = get_node("Particles2D2")
+onready var a1 : AudioStreamPlayer = get_node("AudioStreamPlayer")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	p1.emitting = true
 	p2.emitting = true
+	a1.pitch_scale = rand_range(0.9, 1.1)
+	a1.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if(!p1.emitting && !p2.emitting):
+	if(!p1.emitting && !p2.emitting && !a1.playing):
 		get_parent().remove_child(self)
